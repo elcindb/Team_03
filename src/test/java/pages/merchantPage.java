5d1385facab6e3223ca398dd61d26b9350277df6
Burhan Akyuz <brhnakyuz@gmail.com>
2024-07-09 20:38:46 +0300

    frameworkStructure

---
+++ src/test/java/pages/merchantPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class merchantPage {
+
+    public merchantPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+}
---
+++ src/test/java/tests/US_01.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class US_01 {
+
+
+    @Test
+    public void TC_0101() {
+
+    }
+
+}
