4584912cb8bfb0502f3d8f2f7074e0990cc163b3
XaverNajafova <nurixaver@gmail.com>
2024-07-16 23:38:42 +0400

    xaver yeni

--- src/test/java/pages/adminPage.java
+++ src/test/java/pages/adminPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class adminPage {
     public adminPage(){
         PageFactory.initElements(Driver.getDriver(),this);
@@ -50,91 +52,91 @@ public class adminPage {
     //ulvi end
 
 
-    //xaver start
+    //xaver start Admin pagede Orders bolumu
     //Admin Page-de Orders butonu
     @FindBy (xpath = "//*[text()='Orders'])[1]")
-    public WebElement OrderButton;
+    public WebElement orderButton;
 
     //Admin Page-de AllOrders butonu
     @FindBy (xpath = "//*[text()='All order']")
-    public WebElement AllorderButton;
+    public WebElement allorderButton;
 
     //Orders-ın bölümündeki All Orders-a tıkladıkdan sonra yeni sayfada "AllOrders" başlığı
     @FindBy (xpath = ("(//*[text()='All Orders']) [2]"))
-    public WebElement AllOrdersTitle;
+    public WebElement allOrdersTitle;
 
 
     //Toplam sifarişler için Orders yazısı
     @FindBy (xpath = "(//*[@class='bg-light p-3 mb-3 rounded'])[1]")
-    public WebElement OrdersTextBox;
+    public WebElement ordersTextBox;
 
     //Toplam iptal için Cancel yazısı
     @FindBy (xpath = "(//*[@class='bg-light p-3 mb-3 rounded'])[2]")
-    public WebElement CancelTextBox;
+    public WebElement cancelTextBox;
 
     //Toplam geri iadeler için Refund yazısı
     @FindBy (xpath = "(//*[@class='bg-light p-3 mb-3 rounded'])[3]")
-    public WebElement RefundTextBox;
+    public WebElement refundTextBox;
 
     //Toplam meblağ için Total Orders yazısı
     @FindBy (xpath = "(//*[@class='bg-light p-3 mb-3 rounded'])[4]")
-    public WebElement TotalOrdersBox;
+    public WebElement totalOrdersBox;
 
-    //Tarih seçebilmek için "Start date and End date" yazısı
+    //Date filter (YYYYMMDD)
     @FindBy (xpath = "//*[@class='form-control py-2 border-right-0 border']")
-    public WebElement StartandEnddateBox;
+    public WebElement startAndEndDateBox;
 
-    //Tarih belirlemek için Calendar butonu
+    //Calendar button
     @FindBy (xpath = "//*[@class='zmdi zmdi-calendar-alt']")
-    public WebElement CalendarIcon;
+    public WebElement calendarIcon;
 
     //Filtreleme butonu
     @FindBy (xpath = "(//*[@class='d-flex'])[6]")
-    public WebElement FiltersButton;
+    public WebElement filtersButton;
 
-    //Kullanıcı profil resmi
-    @FindBy (xpath = "(//*[@class='sorting_disabled'])[1]")
-    public WebElement ProfilePicture;
+    //Header bolumunde kullanıcı profil resmi
+    @FindBy(xpath = "(//tr//td)[1]")
+    public List<WebElement> imageProfileX;
 
-    //Kullanıcı bilgileri
-    @FindBy (xpath = "(//*[@class='sorting_disabled'])[2]")
-    public WebElement OrderInformationText;
+    //Siparis bilgileri
+    @FindBy(xpath = "(//tr//td)[2]")
+    public List<WebElement> orderInformationText;
 
     //Siparişlerin ID numarası
-    @FindBy (xpath = "//*[@class='sorting sorting_desc']")
-    public  WebElement OrderID;
+    @FindBy(xpath = "(//tr//td)[3]")
+    public  List<WebElement> orderIDColumn;
 
     //Restoran bilgileri
-    @FindBy (xpath = "(//*[@class='sorting_disabled'])[3]")
-    public WebElement Merchant;
+    @FindBy (xpath = "(//tr//td)[4]")
+    public WebElement merchant;
 
-    //Müsteri bilgileri
-    @FindBy (xpath = "(//*[@class='sorting_disabled'])[4]")
-    public WebElement Customer;
+    //Müsteri ismi
+    @FindBy(xpath = "(//tr//td)[5]")
+    public List<WebElement> customer;
 
     //View ve Download ikonlarının bulunduğu bölüm
     @FindBy (xpath = "(//*[@class='sorting_disabled'])[5]")
-    public WebElement Actions;
+    public WebElement actions;
 
-    //Siparişin detay bilgileri
-    @FindBy (xpath = "(//*[@class='zmdi zmdi-eye'])[1]")
-    public WebElement EyeIcon;
+    //Admin All Orders ilk order satırı inceleme butonu (-index kullanilmali)
+    @FindBy (xpath ="(//tr//td[5]//a)[1]")
+    public List<WebElement> adminAllOrdersFirstOrderObserve;
 
-    //Sipariş bilgilerinin PDF formatında indirilmesi
-    @FindBy (xpath = "(//*[@class='zmdi zmdi-download'])[1]")
-    public WebElement DownloadIcon;
+   //Admin All Orders ilk order satırı indirme butonu (-index kullanilmali)
+    @FindBy (xpath = "(//tr//td[5]//a)[2]")
+    public List<WebElement> adminAllOrdersFirstOrderDownloadButon;
 
     //Toplam giriş sayısını görmek için
     @FindBy (xpath = "//*[@class='dataTables_info']")
-    public WebElement DataTableinfo;
+    public WebElement dataTableinfo;
 
     //Sayfaya geri dönmek için Previous butonu
-    @FindBy (xpath = "//*[@class='paginate_button page-item previous disabled']")
-    public WebElement PreviousButton;
+    @FindBy (xpath = "//*[@class='paginate_button page-item previous']")
+    public List<WebElement> previousButton;
 
     //Diğer sayfaya geçmek için Next butonu
-    @FindBy (xpath = "(//*[@class='page-link'])[9]")
-    public WebElement NextButton;
+    @FindBy (xpath = "//*[@class='paginate_button page-item next']")
+    public List<WebElement> nextButton;
 
     //xaver end
 
