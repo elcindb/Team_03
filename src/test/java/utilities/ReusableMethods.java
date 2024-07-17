2d089dbdc97762ae0b7eb081cbd0451c3b9d608a
emsa90 <emsa90@gmail.com>
2024-07-17 19:53:48 -0400

    Elcin_US2

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,6 +1,7 @@
 package utilities;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.*;
 
@@ -8,10 +9,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.function.Function;
 
 public class ReusableMethods {
